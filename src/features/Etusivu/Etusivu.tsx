95f6f950ccf4eaff6a50759f2036f2fc3968fe7d
TetrisBattle <thien5596@gmail.com>
2023-11-09 12:15:56 +0200

    Replace local video with youtube video

--- src/features/Etusivu/Etusivu.tsx
+++ src/features/Etusivu/Etusivu.tsx
@@ -1,4 +1,3 @@
-import ReactPlayer from 'react-player'
 import { Box, Typography } from '@mui/material'
 import { Summary } from './Summary'
 import { CommentsSection } from './CommentsSection'
@@ -16,13 +15,27 @@ export const Etusivu = () => {
 					mx: 'auto',
 				}}
 			>
-				<ReactPlayer
-					url='videos/mainosvideo.mp4'
-					width='100%'
-					height='auto'
-					controls
-					volume={0.25}
-				/>
+				<Box
+					sx={{
+						position: 'relative',
+						overflow: 'hidden',
+						width: '100%',
+						paddingTop: '56.25%', //  16:9 Aspect Ratio (9/16 = 0.5625)
+					}}
+				>
+					<iframe
+						width='100%'
+						height='100%'
+						src='https://www.youtube.com/embed/i55xJMgmqAY'
+						title='CN hierontapalvelut'
+						allowFullScreen
+						style={{
+							position: 'absolute',
+							inset: 0,
+						}}
+					></iframe>
+				</Box>
+
 				<Typography>Video ja kuvat: Marjo Karinkanta</Typography>
 			</Box>
 			<Guide />
