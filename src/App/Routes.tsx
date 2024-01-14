aaa84304ad906414767e913cc604ade0a6594cb6
TetrisBattle <thien5596@gmail.com>
2024-01-14 13:13:49 +0200

    Remove RouteObject type

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -1,9 +1,9 @@
-import { Navigate, RouteObject, createBrowserRouter } from 'react-router-dom'
+import { Navigate, createBrowserRouter } from 'react-router-dom'
 import { Etusivu } from 'features/Etusivu/Etusivu'
 import { Palvelut } from 'features/Palvelut'
 import { Yhteystiedot } from 'features/Yhteystiedot'
 import { Yritys } from 'features/Yritys'
-import { Root } from './Root'
+import { Root } from 'App/Root'
 
 export enum RouteOption {
 	Etusivu = '/etusivu',
@@ -12,7 +12,7 @@ export enum RouteOption {
 	Yritys = '/yritys',
 }
 
-const routes: RouteObject[] = [
+export const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Root />,
@@ -31,9 +31,7 @@ const routes: RouteObject[] = [
 			{ path: RouteOption.Yritys, element: <Yritys /> },
 		],
 	},
-]
-
-export const router = createBrowserRouter(routes)
+])
 
 export const routeToTitle = (route: RouteOption): string => {
 	return route[1].toUpperCase() + route.slice(2)
